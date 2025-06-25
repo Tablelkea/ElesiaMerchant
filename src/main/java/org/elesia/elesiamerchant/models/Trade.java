8e747572bdbc2f036165630047d8a46278732b4f
TableIkea_ <coubard.k18@gmail.com>
2025-06-25 10:15:39 +0200

    .

--- src/main/java/org/elesia/elesiamerchant/models/Trade.java
+++ src/main/java/org/elesia/elesiamerchant/models/Trade.java
@@ -1,75 +1,162 @@
 package org.elesia.elesiamerchant.models;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.permissions.Permission;
+import org.elesia.elesiamerchant.utils.ItemBuilder;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.UUID;
 
 public class Trade {
 
-    private final List<ItemStack> inputs;
-    private final List<ItemStack> outputs;
+    private List<@NotNull ItemStack> item_inputs;
+    private @Nullable double money_input;
+    private @Nullable Permission needed_permission;
 
-    private final boolean repeatable;
-    private final boolean visibleWhenLocked;
+    private List<@NotNull ItemStack> item_outputs;
+    private @Nullable double money_outputs;
+    private @Nullable Permission output_permission;
 
-    public Trade(List<ItemStack> inputs, List<ItemStack> outputs, boolean repeatable, boolean visibleWhenLocked){
-        this.inputs = inputs;
-        this.outputs = outputs;
-        this.repeatable = repeatable;
-        this.visibleWhenLocked = visibleWhenLocked;
+    private @NotNull int max_uses;
+
+    public Trade(@NotNull List<@NotNull ItemStack> item_inputs, @Nullable double money_input, @Nullable Permission needed_permission, List<@NotNull ItemStack> item_outputs, @Nullable double money_outputs, @Nullable Permission output_permission, @NotNull int max_uses) {
+        this.item_inputs = item_inputs;
+        this.money_input = money_input;
+        this.needed_permission = needed_permission;
+
+        this.item_outputs = item_outputs;
+        this.money_outputs = money_outputs;
+        this.output_permission = output_permission;
+
+        this.max_uses = max_uses;
     }
 
-    public void addToNpc(Npc npc){
-        npc.getTrades().add(this);
+    public @NotNull List<@NotNull ItemStack> getItem_inputs() {
+        return item_inputs;
     }
 
-    public void addToNpc(String name){
-        for(Npc npc : Npc.npc_list){
-            if(npc.getName().equalsIgnoreCase("name")){
-                npc.getTrades().add(this);
+    public void setItem_inputs(@NotNull List<@NotNull ItemStack> item_inputs) {
+        this.item_inputs = item_inputs;
+    }
+
+    public double getMoney_input() {
+        return money_input;
+    }
+
+    public void setMoney_input(double money_input) {
+        this.money_input = money_input;
+    }
+
+    public @Nullable Permission getNeeded_permission() {
+        return needed_permission;
+    }
+
+    public void setNeeded_permission(@Nullable Permission needed_permission) {
+        this.needed_permission = needed_permission;
+    }
+
+    public @NotNull List<@NotNull ItemStack> getItem_outputs() {
+        return item_outputs;
+    }
+
+    public void setItem_outputs(@NotNull List<@NotNull ItemStack> item_outputs) {
+        this.item_outputs = item_outputs;
+    }
+
+    public double getMoney_outputs() {
+        return money_outputs;
+    }
+
+    public void setMoney_outputs(double money_outputs) {
+        this.money_outputs = money_outputs;
+    }
+
+    public @Nullable Permission getOutput_permission() {
+        return output_permission;
+    }
+
+    public void setOutput_permission(@Nullable Permission output_permission) {
+        this.output_permission = output_permission;
+    }
+
+    public int getMax_uses() {
+        return max_uses;
+    }
+
+    public void setMax_uses(int max_uses) {
+        this.max_uses = max_uses;
+    }
+
+    public boolean isConvertibleToVillagerTrade(){
+        return this.getItem_inputs().size() <= 2 && this.getItem_outputs().size() == 1 && this.money_input == 0 && this.money_outputs == 0 && this.needed_permission == null && this.output_permission == null;
+    }
+
+    public MerchantRecipe convertToVillagerTrade(){
+        if(isConvertibleToVillagerTrade()){
+            MerchantRecipe recipe = new MerchantRecipe(this.item_outputs.getFirst(), this.max_uses);
+            recipe.addIngredient(this.item_inputs.getFirst());
+            if(this.item_inputs.size() == 2){
+                recipe.addIngredient(this.item_inputs.get(1));
+            }
+            return recipe;
+        }
+
+        return null;
+    }
+
+    public void convertToComplexeTrade(Inventory inventory){
+        if(!this.isConvertibleToVillagerTrade()){
+            for(int i = 0; i < 9; i++){
+                if(inventory.getItem(i) == null){
+                    double moneyRequire = this.money_input;
+                    double moneyTrade = this.money_outputs;
+
+                    List<ItemStack> inputs = this.item_inputs;
+                    List<ItemStack> outputs = this.item_outputs;
+
+                    Permission neededPermission = this.needed_permission;
+                    Permission tradePermission = this.output_permission;
+
+                    // OUT-PUT
+
+                    if(outputs.size() > 1 || (moneyTrade > 0 && outputs.size() == 1) ){
+                        inventory.setItem(i, new ItemBuilder(Material.CHEST, 1, "Outputs Content", null).toItemStack());
+                    }else{
+                        if(outputs.size() == 1 && moneyTrade == 0){
+                            inventory.setItem(i, outputs.getFirst());
+                        }else if(outputs.isEmpty() && moneyTrade > 0){
+                            inventory.setItem(i, new ItemBuilder(Material.PAPER, 1, "Outputs Money:" + (int) moneyTrade, null).toItemStack());
+                        }
+                    }
+
+                    // IN-PUT
+
+                    if(inputs.size() > 2 || (inputs.size() == 2 && money_input > 0)){
+                        inventory.setItem(i+27, new ItemBuilder(Material.CHEST, 1, "Inputs Content", null).toItemStack());
+                    }else{
+                        if(moneyTrade == 0){
+                            if(inputs.size() == 1){
+                                inventory.setItem(i+27, inputs.getFirst());
+                            }else if(inputs.size() == 2){
+                                inventory.setItem(i+27, inputs.getFirst());
+                                inventory.setItem(i+18, inputs.get(1));
+                            }
+                        }else{
+                            if(inputs.size() == 1){
+                                inventory.setItem(i+27, inputs.getFirst());
+                                inventory.setItem(i+18, new ItemBuilder(Material.PAPER, 1, "Outputs Money:" + (int) moneyTrade, null).toItemStack());
+                            }else if(inputs.size() == 2){
+                                inventory.setItem(i+27, new ItemBuilder(Material.CHEST, 1, "Inputs Content", null).toItemStack());
+                            }
+                        }
+                    }
+
+                }
             }
         }
     }
-
-    public void addToNpc(UUID uuid){
-        for(Npc npc : Npc.npc_list){
-            if(npc.getUniqueId().equals(uuid)){
-                npc.getTrades().add(this);
-            }
-        }
-    }
-
-    public List<ItemStack> getInputs(){
-        return this.inputs;
-    }
-
-    public List<ItemStack> getOutputs(){
-        return this.outputs;
-    }
-
-    public boolean isRepeatable(){
-        return this.repeatable;
-    }
-
-    public boolean isVisibleWhenLocked(){
-        return this.visibleWhenLocked;
-    }
-
-    public static List<ItemStack> getInputs(Trade trade){
-        return trade.getInputs();
-    }
-
-    public static List<ItemStack> getOutputs(Trade trade){
-        return trade.getOutputs();
-    }
-
-    public static boolean isRepeatable(Trade trade){
-        return trade.isRepeatable();
-    }
-
-    public static boolean isVisibleWhenLocked(Trade trade){
-        return trade.isVisibleWhenLocked();
-    }
-
 }
